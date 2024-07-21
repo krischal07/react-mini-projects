bd2688a850d866a2f7896279a88106eebac40beb
krischal07 <krischal.shrestha9849@gmail.com>
2024-07-21 23:20:48 +0545

    Created localstorage in navbar

--- react-navbar/src/App.jsx
+++ react-navbar/src/App.jsx
@@ -1,8 +1,13 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from './components/Navbar/Navbar'
 
 export default function App() {
-  const [theme, setTHeme] = useState('light');
+  const currentTheme = localStorage.getItem('current_theme');
+  const [theme, setTHeme] = useState(currentTheme?currentTheme:'light');
+
+  useEffect(()=>{
+    localStorage.setItem('current_theme', theme);
+  },[theme])
   return (
     <div className={`container ${theme}`}>
       <Navbar theme={theme} setTheme={setTHeme}/>
